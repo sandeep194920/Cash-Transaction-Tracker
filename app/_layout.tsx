b6efac0220777fd14ee61d3765d359a6ba297320
sandeep194920 <sandeepamaranath@gmail.com>
2024-01-01 11:22:20 +0530

    realm installed and errors cleared

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,44 @@
 import { Stack } from 'expo-router/stack'
 import AppContext from '../utils/AppContext'
+import { RealmProvider } from '@realm/react'
 
 export default function Layout() {
   return (
-    <AppContext>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerTitle: 'Customers',
-            headerStyle: {
-              backgroundColor: '#030303',
-            },
-            headerTintColor: 'white',
-          }}
-        />
-        <Stack.Screen
-          name="customers/[customer_id]"
-          options={{
-            headerTitle: 'Transactions',
-            headerStyle: {
-              backgroundColor: '#191d1d',
-            },
-            headerTintColor: 'white',
-          }}
-        />
-        <Stack.Screen
-          name="customers/orders/[order_id]"
-          options={{
-            headerTitle: 'Details',
-            headerStyle: {
-              backgroundColor: '#191d1d',
-            },
-            headerTintColor: 'white',
-          }}
-        />
-      </Stack>
-    </AppContext>
+    <RealmProvider>
+      <AppContext>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerTitle: 'Customers',
+              headerStyle: {
+                backgroundColor: '#030303',
+              },
+              headerTintColor: 'white',
+            }}
+          />
+          <Stack.Screen
+            name="customers/[customer_id]"
+            options={{
+              headerTitle: 'Transactions',
+              headerStyle: {
+                backgroundColor: '#191d1d',
+              },
+              headerTintColor: 'white',
+            }}
+          />
+          <Stack.Screen
+            name="customers/orders/[order_id]"
+            options={{
+              headerTitle: 'Details',
+              headerStyle: {
+                backgroundColor: '#191d1d',
+              },
+              headerTintColor: 'white',
+            }}
+          />
+        </Stack>
+      </AppContext>
+    </RealmProvider>
   )
 }
